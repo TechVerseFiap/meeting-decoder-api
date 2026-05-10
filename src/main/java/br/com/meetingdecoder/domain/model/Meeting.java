99f1809c03355b369fe1445c2ac239175a57ddb9
Davi Munhoz <davimunhoz1005@gmail.com>
2026-05-10 19:22:38 -0300

    feat/domain-structure: create meeting entity

---
+++ src/main/java/br/com/meetingdecoder/domain/model/Meeting.java
@@ -0,0 +1,71 @@
+package br.com.meetingdecoder.domain.model;
+
+import br.com.meetingdecoder.domain.validation.DomainValidation;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Meeting {
+    private UUID id;
+    private UUID clientId;
+    private UUID sellerId;
+    private LocalDateTime timestamp;
+
+    private Meeting(
+            UUID clientId,
+            UUID sellerId,
+            LocalDateTime timestamp
+    ) {
+        validate(clientId, sellerId, timestamp);
+
+        this.id = UUID.randomUUID();
+        this.clientId = clientId;
+        this.sellerId = sellerId;
+        this.timestamp = timestamp;
+    }
+
+    private Meeting(
+            UUID clientId,
+            UUID sellerId
+    ) {
+        validate(clientId, sellerId);
+
+        this.id = UUID.randomUUID();
+        this.clientId = clientId;
+        this.sellerId = sellerId;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    private void validate(
+            UUID clientId,
+            UUID sellerId,
+            LocalDateTime timestamp
+    ) {
+        DomainValidation.notNull(clientId, "clientId");
+        DomainValidation.notNull(sellerId, "sellerId");
+        DomainValidation.notFuture(timestamp, "timestamp");
+    }
+
+    private void validate(
+            UUID clientId,
+            UUID sellerId
+    ) {
+        DomainValidation.notNull(clientId, "clientId");
+        DomainValidation.notNull(sellerId, "sellerId");
+    }
+
+    public static Meeting create (
+            UUID clientId,
+            UUID sellerId,
+            LocalDateTime timestamp
+    ) {
+        return new Meeting (clientId, sellerId, timestamp);
+    }
+
+    public static Meeting create (
+            UUID clientId,
+            UUID sellerId
+    ) {
+        return new Meeting (clientId, sellerId);
+    }
+}
