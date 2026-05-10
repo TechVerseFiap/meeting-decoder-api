e25049c0e00c1de7216bbf932a44c4fc2b9fb041
Davi Munhoz <davimunhoz1005@gmail.com>
2026-05-10 14:23:14 -0300

    feat/setup: initialize spring boot api project

---
+++ src/main/java/br/com/meetingdecoder/MeetingDecoderApplication.java
@@ -0,0 +1,12 @@
+package br.com.meetingdecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingDecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingDecoderApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/meetingdecoder/MeetingDecoderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meetingdecoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingDecoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
