f592d731251dec4ba14cb7191bca1bd35da04525
Davi Munhoz <davimunhoz1005@gmail.com>
2026-05-17 13:34:06 -0300

    feat/ports-input: implement the ServiceRecommendationUseCase granularity to truly follow the Use Case concept

---
+++ src/main/java/br/com/meetingdecoder/application/ports/in/serviceRecommendation/GetServiceRecommendationUseCase.java
@@ -0,0 +1,11 @@
+package br.com.meetingdecoder.application.ports.in.serviceRecommendation;
+
+import br.com.meetingdecoder.application.dto.ServiceRecommendationResponse;
+import br.com.meetingdecoder.application.shared.result.Result;
+
+import java.util.UUID;
+
+public interface GetServiceRecommendationUseCase {
+    Result<ServiceRecommendationResponse> byId(UUID id);
+    Result<ServiceRecommendationResponse> byName(String name);
+}
