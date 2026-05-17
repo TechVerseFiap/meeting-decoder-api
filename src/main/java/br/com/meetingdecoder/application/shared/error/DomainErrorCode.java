acb7a9fffb20b923f323bc4935d75e94d7e67c61
Davi Munhoz <davimunhoz1005@gmail.com>
2026-05-17 01:01:23 -0300

    refactor/result-pattern: implement DomainErrorCode in this branch

---
+++ src/main/java/br/com/meetingdecoder/application/shared/error/DomainErrorCode.java
@@ -0,0 +1,9 @@
+package br.com.meetingdecoder.application.shared.error;
+
+public enum DomainErrorCode {
+    EMPTY_FIELD,
+    INVALID_PRICE,
+    INVALID_DATE,
+    ALREADY_EXISTS,
+    NOT_FOUND
+}
