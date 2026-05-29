2a385923585dc01718ed60a0e94f9ccef66cba33
Davi Munhoz <davimunhoz1005@gmail.com>
2026-05-29 16:44:47 -0300

    feat/transcription-context: create transcription repository interface

---
+++ src/main/java/br/com/meetingdecoder/domain/transcription/repository/ITranscriptionRepository.java
@@ -0,0 +1,17 @@
+package br.com.meetingdecoder.domain.transcription.repository;
+
+import br.com.meetingdecoder.domain.transcription.model.Transcription;
+import br.com.meetingdecoder.domain.transcription.valueobject.MeetingId;
+import br.com.meetingdecoder.domain.transcription.valueobject.TranscriptionId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITranscriptionRepository {
+    Transcription save(Transcription client);
+    boolean existsById(TranscriptionId id);
+    boolean existsByName(String name);
+    List<Transcription> findAll();
+    Optional<Transcription> findById(MeetingId id);
+    void deleteById(MeetingId id);
+}
